8eae7d99a68e4a7d650446f7b10472f0fb1bdc96
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2026-06-03 22:10:12 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var promedio = new Promedio();
+
+Console.WriteLine("Ingrese números para calcular el promedio (ingrese 0 o menos para finalizar):");
+while (true){
+    Console.Write("Número: ");
+    var input = Console.ReadLine();
+    if (double.TryParse(input, out double numero))    {
+        if (numero <= 0) break;
+        promedio.AddNumero(numero);
+    } else {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número.");
+    }
+}
+Console.WriteLine("------------------------------");
+Console.WriteLine($"El promedio es: {promedio.CalcularPromedio()}");
